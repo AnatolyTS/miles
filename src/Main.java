7be02bfa8eaee817ee926a1c4c8b9fb92cecee3a
Anatoly Tsybrov <ttoljke@gmail.com>
2020-10-30 21:13:57 +0300

    мили

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 25000;
+        int rub = 20;
+        int bonus = 1;
+        int mili = (price / rub) * bonus;
+        System.out.println(mili);
+
+    }
+
+}
